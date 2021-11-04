2dd8a70c5d8d2a546a478ecbe82973369a0d5d5c
Joshua Littles <littles.j.i.i@gmail.com>
2021-11-04 17:01:33 -0400

    work in transferService

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,5 +1,17 @@
 package com.techelevator.tenmo.services;
 
-public class TransferService {
+import org.springframework.web.client.RestTemplate;
 
+public class TransferService {
+    private final String API_BASE_URL;
+    private final String API_KEY;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public TransferService(String api_base_url, String api_key, RestTemplate restTemplate) {
+        API_BASE_URL = api_base_url;
+        API_KEY = api_key;
+        this.restTemplate = restTemplate;
+    }
+
+    
 }
